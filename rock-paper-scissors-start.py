57885abbb95b611b272cb53b7f5c15cd9d9e61cf
mhsiao345 <72826525+mhsiao345@users.noreply.github.com>
2021-02-14 15:10:27 -0800

    Add files via upload

---
+++ rock-paper-scissors-start.py
@@ -0,0 +1,49 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+var1 = int(input('What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n'))
+
+print(game_images[var1])
+
+import random
+var2 = random.randint(0,2)
+print('Computer chose:')
+
+print(game_images[var2])
+
+if var1 == var2:
+  print("It's a tie.")
+elif var1 == 0 and var2 == 1:
+  print('You lose.')
+elif var1 == 1 and var2 == 2:
+  print('You lose.')
+elif var1 == 2 and var2 == 0:
+  print('You lose.')
+else:
+  print('You win.')
